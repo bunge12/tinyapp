9db0182f0c6c1a9eee3f49dadf070ffdba8e1c0c
Artur Iatsko <arturyatsko@gmail.com>
2020-02-18 22:34:38 +0000

    Fixed username handling and updated style

--- express_server.js
+++ express_server.js
@@ -30,6 +30,7 @@ app.get("/u/:shortURL", (req, res) => {
   }
   else { res.redirect(longURL); }
 });
+
 app.get(["/urls", "/"], (req, res) => {
   let templateVars = {
     urls: urlDatabase,
@@ -37,16 +38,26 @@ app.get(["/urls", "/"], (req, res) => {
   };
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 app.post("/urls/:shortURL", (req, res) => {
