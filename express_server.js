7146586b5627f27d6233d4f06ae1d2d141ab6cac
Artur Iatsko <arturyatsko@gmail.com>
2020-02-18 22:54:03 +0000

    Register form added

--- express_server.js
+++ express_server.js
@@ -39,6 +39,13 @@ app.get(["/urls", "/"], (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("register", templateVars);
+});
+
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     username: req.cookies["username"]
