79b2a7c62a2c00ce4a4bda4dcddb342c4f0128f8
Artur Iatsko <arturyatsko@gmail.com>
2020-02-18 00:02:32 +0000

    New URLs form added & installed body-parser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -29,6 +32,9 @@ app.get("/fetch", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
